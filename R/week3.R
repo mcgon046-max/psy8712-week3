3a9e1e40a2f7eccc3b11fce455301dd1c7b681cc
Patrick Mcgonagle <mcgon046@umn.edu>
2026-02-08 17:13:29 -0600

    Saving progress

--- R/week3.R
+++ R/week3.R
@@ -1,3 +1,10 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # Leave empty? not included in instructions. 
+# Data Import and Cleaning
+raw_df <- read.csv(file = "../data/week3.csv", header = TRUE)
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
+clean_df <- raw_df[format(raw_df$timeStart, "%Y-%m") != "2017-06", ]
+clean_df <- clean_df[clean_df$q6 == 1,]
+# Left empty, not included in instructions 
