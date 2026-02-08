e4b65c212e508c8179a0b9936b20b754a77b3b00
Patrick Mcgonagle <mcgon046@umn.edu>
2026-02-08 17:34:45 -0600

    saving

--- R/week3.R
+++ R/week3.R
@@ -13,3 +13,6 @@ clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, units = "se
 clean_df$timeSpent <- as.numeric(clean_df$timeSpent); hist(clean_df$timeSpent)
 frequency_tables_list <- lapply(clean_df[,5:14], table)
 lapply(frequency_tables_list, barplot)
+sum(clean_df$q1 >= clean_df$q2 & clean_df$q2 == clean_df$q3)
+for (i in 1:frequency_tables_list) { 
+}
