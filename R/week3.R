f4976af49c2d3f2dbc4bf56f9238ebbfb4aea858
Patrick Mcgonagle <mcgon046@umn.edu>
2026-02-08 17:26:35 -0600

    lapply not working, but we'll get there

--- R/week3.R
+++ R/week3.R
@@ -11,4 +11,4 @@ clean_df <- clean_df[clean_df$q6 == 1,]
 # Analysis 
 clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, units = "secs")
 clean_df$timeSpent <- as.numeric(clean_df$timeSpent); hist(clean_df$timeSpent)
-frequency_tables_list <- 
+frequency_tables_list <- lapply()
